3d3bd9f5f67e2c1c3a5e01501cd610f937ed679d
Andrew Miller <amiller@metabolon.com>
2018-08-28 11:10:24 -0400

    Example 2

---
+++ Example.02/Users.Roles/UserRolesService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// <author>Billy Gates</author>
+/// </summary>
+namespace Users.Roles
+{
+    /// <summary>
+    /// TODO: Extract an interface
+    /// </summary>
+    public class UserRolesService
+    {
+        public IEnumerable<string> FindUsersByRole(string roleName)
+        {
+            var mockUserRoles = new Dictionary<string, List<string>>
+            {
+                { "admin", new List<string>(new string[] { "billy" }) },
+                { "gues", new List<string>(new string[] { "donald" }) },
+                { "writer", null }
+            };
+
+            return mockUserRoles[roleName];
+        }
+    }
+}
