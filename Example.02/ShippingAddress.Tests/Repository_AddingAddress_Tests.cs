1b535f7497460445dfd77af6773603da2e6912b6
Dmitrii Maliukov <dmitrii.maliukov@gmail.com>
2018-08-29 11:13:01 -0400

    Added address validation example with tests (#4)

---
+++ Example.02/ShippingAddress.Tests/Repository_AddingAddress_Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Xunit;
+
+namespace ShippingAddress.Tests
+{
+    public class Repository_AddingAddress_Tests
+    {
+        private readonly Address _address = new Address();
+
+        [Fact]
+        public void AddingAddress_AddressValid()
+        {
+            var alwaysExistingVerifier = new Moq.Mock<IAddressVerifier>();
+            alwaysExistingVerifier.Setup(x => x.Verify(_address)).Returns(() => { return true; });
+
+            var repo = new InMemoryRepository(alwaysExistingVerifier.Object);
+            repo.AddAddress(_address);
+
+            Assert.Single(repo.ShippingAddresses);
+            Assert.True(repo.ShippingAddresses.First().Existed);
+        }
+
+        [Fact]
+        public void AddingAddress_AddressNotValid()
+        {
+            var alwaysNotExistingVerifier = new Moq.Mock<IAddressVerifier>();
+            alwaysNotExistingVerifier.Setup(x => x.Verify(_address)).Returns(() => { return false; });
+
+            var repo = new InMemoryRepository(alwaysNotExistingVerifier.Object);
+            repo.AddAddress(_address);
+
+            Assert.Single(repo.ShippingAddresses);
+            Assert.False(repo.ShippingAddresses.First().Existed);
+        }
+
+        [Fact]
+        public void AddingAddress_ServiceException()
+        {
+            var alwaysExceptionVerifier = new Moq.Mock<IAddressVerifier>();
+            alwaysExceptionVerifier.Setup(x => x.Verify(_address)).Returns(() => { throw new WebException("500 Internal Server Error"); });
+
+            var repo = new InMemoryRepository(alwaysExceptionVerifier.Object);
+            repo.AddAddress(_address);
+
+            Assert.Single(repo.ShippingAddresses);
+        }
+    }
+}
---
+++ Example.02/ShippingAddress/InMemoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ShippingAddress
+{
+    public class InMemoryRepository
+    {
+        private readonly IAddressVerifier _addressVerifier;
+
+        public List<Address> ShippingAddresses { get; private set; }
+
+        public InMemoryRepository(IAddressVerifier addressVerifier)
+        {
+            _addressVerifier = addressVerifier;
+
+            ShippingAddresses = new List<Address>();
+        }
+
+        public void AddAddress(Address address)
+        {
+            address.Existed = _addressVerifier.Verify(address);
+
+            ShippingAddresses.Add(address);
+        }
+    }
+}
